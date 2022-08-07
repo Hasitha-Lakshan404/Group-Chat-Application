2d29dc2dc9d4f0965ad07baf66325f5ed31272e1
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-08-07 16:54:49 +0530

    Create Server

---
+++ src/server/Server.java
@@ -0,0 +1,26 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :Group Chat Application
+ * Date :8/7/2022
+ * Time :4:42 PM
+ */
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket=new ServerSocket(5000);
+        Socket accept;
+
+        System.out.println("Waiting for Client ...");
+        accept= serverSocket.accept();
+        System.out.println("Client Connected");
+
+
+    }
+}
